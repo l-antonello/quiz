f01b1ead536c64d400f4bf2a9e2bdd9b5baec075
l-antonello <122548483+l-antonello@users.noreply.github.com>
2023-05-07 19:53:27 -0400

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,75 +1,55 @@
-// Quiz questions and answersvarvar
-var questions = [{
-    question: "What is the capital of Pennsylvania?",
-    choices: ["Philadelphia", "Harrisburg", "Pittsburgh", "Allentown"],
-    correctAnswer: "Harrisburg"
-  },
-  {
-    question: "What is the State dog of Pennsylvania?",
-    choices: ["Dachshund", "Chihuahua", "Golden Retriever", "Great Dane"],
-    correctAnswer: "Great Dane"
-  },
-  {
-    question: "What river separates New Jersey from Philadelphia?",
-    choices: ["Yellow", "Schuylkill", "Delaware", "Houston"],
-    correctAnswer: "Delaware"
-  }
-];
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Code Quiz Challenge</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+    <link rel="stylesheet" type="text/css" href="./assets/style.css">
+</head>
+<body>
+    <div class="container">
 
-// Timer
-var timeEl = document.querySelector(".time");
+        <div id="startpage" class="section">
+            <h2 class="title">Quiz on Pennsylvania and Philadelphia!</h2>
+            <button id="startbtn" class="button is-primary">Start Quiz</button>
+            <button id="startPageHighscore" class="button is-dark" onclick="showHighscore()">High Scores</button>
+        </div>
 
-var mainEl = document.getElementById("main");
+        <div id="quiz" class="section">
+            <div id="timer">Time left: </div>
+            <div id="questions">
+                <p>Questions Placeholder</p>
+            </div>
+            <button id="a" class="button" onclick="checkAnswer('a')">A</button>
+            <button id="b" class="button" onclick="checkAnswer('b')">B</button>
+            <button id="c" class="button" onclick="checkAnswer('c')">C</button>
+            <button id="d" class="button" onclick="checkAnswer('d')">D</button>
+            <div id="result"></div>
+        </div>
 
-var secondsLeft = 15;
+        <div id="gameover" class="section">
+            <div id="finalScore"></div>
+            <input type="text" size="40" placeholder="Enter your initials for high score" name="initials" id="initials" class="input">
+            <button id="submitScore" class="button is-primary">Submit Score</button>
+        </div>
 
-function setTime() {
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timeEl.textContent = secondsLeft + "seconds untill game over!.";
+        <div id="highscoreContainer" class="section">
+            <div id="high-scorePage">
+                <div id="highscore-header" class="columns">
+                    <div class="column"><h3>User Initials</h3></div>
+                    <div class="column"><h3>Score</h3></div>
+                </div>
+                <div id="highscore-initials"></div>
+                <div id="highscore-score"></div>
+            </div>
+            <div id="endGameBtns" class="section">
+                <button id="playAgain" class="button is-primary" onclick="replayQuiz()">Play Again</button>
+                <button id="clearHighscore" class="button is-danger" onclick="clearScore()">Clear Highscores</button>
+            </div>
+        </div>
 
-    if(secondsLeft === 0) {
-  
-      clearInterval(timerInterval);
-     
-     alert("game over :(");
-    }
-
-  }, 1000);
-}
-setTime();
-
-
-// DOM elements
-var startBtn = document.getElementById("startBtn");
-var quizContainer = document.getElementById("quizContainer");
-var questionText = document.getElementById("questionText");
-var choicesList = document.getElementById("choicesList");
-var submitBtn = document.getElementById("submitBtn");
-var resultContainer = document.getElementById("resultContainer");
-var resultText = document.getElementById("resultText");
-var initialsInput = document.getElementById("initialsInput");
-var saveBtn = document.getElementById("saveBtn");
-
-// Event listener for start button
-startBtn.addEventListener("click", startQuiz);
-
-// Event listener for submit button
-submitBtn.addEventListener("click", submitAnswer);
-
-// Event listener for save button
-saveBtn.addEventListener("click", saveScore);
-
-// Function to start quiz
-function startQuiz() {
-  startBtn.style.display = "none";
-  quizContainer.style.display = "block";
-  loadQuestion();
-  startTimer();
-}
-
-// Function to load question
-function loadQuestion() {
-    var currentQuestion = questions[currentQuestionIndex];
-    questionText.textContent
-}
+    </div>
+    <script src="./assets/script.js"></script>
+</body>
+</html>
